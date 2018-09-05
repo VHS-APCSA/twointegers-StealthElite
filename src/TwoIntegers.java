ebca37ee1d0714d7e46c02b7095acc8209a4f1f7
bob <bob@APCSA>
2018-09-05 14:49:43 -0700

    TwoIntegers

--- src/TwoIntegers.java
+++ src/TwoIntegers.java
@@ -1,4 +1,32 @@
-
-public class TwoIntegers {
+public class TwoIntegers 
+{
+	private int num1;
+	private int num2;
+	
+	public TwoIntegers(int num1 , int num2)
+	{
+		this . num1 = num1;
+		this . num2 = num2;
+	}
+	public void setNum1(int num1)
+	{
+		this . num1 = num1;
+	}
+	public int getNum1()
+	{
+		return num1;
+	}
+	public void setNum2(int num2)
+	{
+		this . num2 = num2;
+	}
+	public int getNum2()
+	{
+		return num2;
+	}
+	
+	
+	
+	
 
 }
