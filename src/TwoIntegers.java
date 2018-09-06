dda2860af5a32572d240280cacdc89f04dae6bcf
bob <bob@APCSA>
2018-09-06 15:39:16 -0700

    TwoIntegers and TwoIntegersRunner

--- src/TwoIntegers.java
+++ src/TwoIntegers.java
@@ -2,7 +2,11 @@ public class TwoIntegers
 {
 	private int num1;
 	private int num2;
-	
+	public TwoIntegers()
+	{
+		num1 = 0;
+		num2 = 1;
+	}
 	public TwoIntegers(int num1 , int num2)
 	{
 		this . num1 = num1;
